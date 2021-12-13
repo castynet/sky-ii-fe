3c76f1cbb9ec6c89dbb98c9066161100234e11ce
castynet <riungemaina@gmail.com>
2021-12-13 13:03:29 +0300

    closes #2

---
+++ src/app/components/sampleComponent/styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  color: var(--purple);
+`;
