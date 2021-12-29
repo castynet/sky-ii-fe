f38433d14733685d0dcc9ad26a558249e8a81f7a
LuffyDrago <rigelvicky@gmail.com>
2021-12-29 22:46:15 +0300

    heading and card

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+
+//import from components
+import Card from "./components/Cardscomponent/Card";
+//import from elements
+import Heading from "./elements/HeaderBanner/Heading";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Card />
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/app/elements/HeaderBanner/Heading.js
@@ -0,0 +1,21 @@
+import React from "react";
+import HeadingCard from "./HeadingCard";
+
+const Heading = () => {
+  return (
+    <>
+      <section className="article-section">
+        <div className="article-all">
+          <HeadingCard
+            title="Shop Grid Default"
+            author=". Shop Grid Default"
+            heading="Home ."
+            details="Pages "
+          />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Heading;
