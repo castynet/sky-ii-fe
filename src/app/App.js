2f7a9cb8f9556b21e4c2470822c3a99969785c09
Victor Mwangi <46398938+LuffyDrago@users.noreply.github.com>
2021-12-29 23:20:47 +0300

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import HeadingBar from "./Components/HeadingBar/HeadingBar";
 import Card from "./components/Cardscomponent/Card";
 //import from elements
 import Heading from "./elements/HeaderBanner/Heading";
-function App() {
+export default function App() {
   return (
     <div>
      <HeadingBar />
