55bb1576feaebcc7251654395ab4ed66bffee857
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-21 10:23:43 +0300

    Latest Updates banner

---
+++ src/app/components/latestUpdates/LatestUpdates.spec.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LatestUpdates from ".LatestUpdates";
+import { StateProvider } from "app/Context";
+import { BrowserRouter } from "react-router-dom";
+
+test("is all the content present in LatestUpdates?", () => {
+  render(
+    <StateProvider>
+      <BrowserRouter>
+        <LatestUpdates />
+      </BrowserRouter>
+    </StateProvider>
+  );
+  expect(screen.getByText("Get Latest Updates By Subscribing")).toBeInTheDocument();
+  expect(screen.getByText("Shop Now")).toBeInTheDocument();
+  expect(screen.getByTestId("To Our Newsletter")).toBeInTheDocument();
+});
---
+++ src/app/components/latestUpdates/Styles.LatestUpdates.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 100vw;
+  overflow: hidden;
+  margin: 10px 0;
+  font-family: var(--heading-font);
+`
+
